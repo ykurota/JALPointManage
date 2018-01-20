d9d10dd180b34f82bc4885e3275b721e326bcb5e
黒谷幸弘 <kurotaniyukihiro@kurotaniyukihironoMacBook-Pro.local>
2018-01-20 15:59:38 +0900

    ログアウト後の遷移先対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_out_path_for(resource)
-    new_user_session_path
+    #new_user_session_path
+    '/jalpoint'
   end
 
 end
